567e6ce9f12c8ae3fe606a86e7a0df63d70b961f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-05 15:28:04 +0330

    Working on index

---
+++ auth/index.php
@@ -0,0 +1,38 @@
+<?php
+
+include('core.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ورود به پنل</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
+    <link href="../pack/css/login.css" rel="stylesheet" rel="text/css">
+</head>
+<body class="main">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 half overflow-auto">
+                <form method="POST" action="index.php">
+                    <label class="form-label" for="username">نام کاربری</label>
+                    <input type="text" class="form-control sborder" id="username" name="username" placeholder="نام کاربری">
+                    <br>
+                    <label class="form-label" for="password">رمز عبور</label>
+                    <input type="password" class="form-control sborder" id="password" name="password" placeholder="رمز عبور">
+                    <br>
+                    <button class="btn sbtn" name="login" type="submit">ورود به پنل</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
