2682d2018d7be8486a60a3225b480358b7683d88
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-05 13:58:48 +0330

    Add configs

---
+++ pack/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Database
+$server = "127.0.0.1";
+$user = "amir";
+$password = "amir";
+$database = "blog";
+
+// Connection
+$connection = mysqli_connect($server, $user, $password, $database);
+
+// URLs
+$host = "localhost";
