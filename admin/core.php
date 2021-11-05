356f4290451fe28f0353a2a35957fb8dfe8a3f81
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-05 19:37:44 +0330

    Add update post status

--- admin/core.php
+++ admin/core.php
@@ -70,3 +70,50 @@ if (isset($_GET['delete'])) {
         <?php
     }
 }
+
+if (isset($_GET['status'])) {
+    $post = $_GET['status'];
+
+    $get_post_query = "SELECT * FROM posts WHERE `row` = '$post'";
+    $get_post_result = mysqli_query($connection, $get_post_query);
+    $get_post_all = mysqli_fetch_assoc($get_post_result);
+
+    if ($get_post_all['status'] == 'public') {
+        $update = "UPDATE posts SET `status` = 'private' WHERE `row` = '$post'";
+        if (mysqli_query($connection, $update)) {
+            ?>
+            <script>
+                window.alert("وضعیت پست تغییر کرد");
+                window.location.replace(".");
+            </script>
+            <?php
+        }
+        else {
+            ?>
+            <script>
+                window.alert("<?php echo mysqli_error($connection); ?>");
+                window.location.replace(".");
+            </script>
+            <?php
+        }
+    }
+    else if ($get_post_all['status'] == 'private') {
+        $update = "UPDATE posts SET `status` = 'public' WHERE `row` = '$post'";
+        if (mysqli_query($connection, $update)) {
+            ?>
+            <script>
+                window.alert("وضعیت پست تغییر کرد");
+                window.location.replace(".");
+            </script>
+            <?php
+        }
+        else {
+            ?>
+            <script>
+                window.alert("<?php echo mysqli_error($connection); ?>");
+                window.location.replace(".");
+            </script>
+            <?php
+        }
+    }
+}
