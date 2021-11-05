4eda6344ec8936e3b4f7dc3812293dfaf7c5dee5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-05 20:10:06 +0330

    Create 404 page

---
+++ errors/404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>پیدا نشد</title>
+    <style>
+        body {
+            text-align: center;
+            direction: rtl;
+            padding-top: 10%;
+            font-size: 25px;
+
+        }
+    </style>
+</head>
+<body>
+    <span>
+        متاسفانه مطلب مورد نظر پیدا نشد
+    </span>
+</body>
+</html>
