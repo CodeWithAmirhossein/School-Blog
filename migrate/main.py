f85ce7f49ce25e6c9c6100513c0d210340f0940d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-05 21:15:59 +0330

    Workin on migrate

--- migrate/main.py
+++ migrate/main.py
@@ -1,4 +1,16 @@
-create_dataase = "CREATE DATABASE blog"
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="amir",
+    password="amir"
+)
+
+cursor = connection.cursor()
+
+cursor.execute("CREATE DATABASE blog")
 
 db_file = open("pack/etc/db.sql", "r")
 table = db_file.read()
+
+cursor.execulte(table)
