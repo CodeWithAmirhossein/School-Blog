8593282ae46c32baa92d6ec20dd33009d67f8065
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-05 19:27:11 +0330

    Fix get post query

--- post/index.php
+++ post/index.php
@@ -7,11 +7,12 @@ include("../pack/config/config.php");
 if (isset($_GET['id'])) {
     $post_id = $_GET['id'];
 
-    $get_post_query = "SELECT * FROM posts WHERE id = '$post_id'";
+    $get_post_query = "SELECT * FROM posts WHERE `row` = '$post_id'";
     $get_post_result = mysqli_query($connection, $get_post_query);
 
     if (mysqli_num_rows($get_post_result) == 1) {
         $post = mysqli_fetch_assoc($get_post_result);
+        echo $post['content'];
     }
     else {
         ?>
