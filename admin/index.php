0312dd35645c1084c8bce18644246269442fe8f7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-05 16:58:11 +0330

    Working on front end

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,62 @@
 <?php
 
 include('core.php');
+
+$get_posts_query = "SELECT * FROM posts";
+$get_posts_result = mysqli_query($connection, $get_posts_query);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>پنل مدیر</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
+    <link href="../pack/css/panel.css" rel="stylesheet" rel="text/css">
+</head>
+<body class="main">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>پست جدید</h3>
+                <br>
+                <div class="dialog">
+                    <input type="text" name="title" placeholder="موضوع" class="form-control">
+                    <br>
+                    <textarea rows="8" class="form-control" name="content" placeholder="پست را اینجا بنویسید . . ."></textarea>
+                    <br>
+                    <button class="btn btn-primary" name="post" type="submit">انتشار</button>
+                </div>
+                <br>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-12">
+                <h3>مدیریت پست ها</h3>
+                <br>
+                <div class="dialog">
+                    <?php
+                    if (mysqli_num_rows($get_posts_result) != 0) {
+                        while ($post = mysqli_fetch_assoc($get_posts_result)) {
+                            echo $post['title'];
+                        }
+                    }
+                    else {
+                        echo "پست جدیدی ثبت نشده است.";
+                    }
+                    ?>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
